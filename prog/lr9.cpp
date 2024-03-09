780bbf94aba7c20671fbfdef3fa4d95ec7a0485d
SperanskyArtyom <awesomepinkieguy@gmail.com>
2024-03-09 22:33:19 +0700

    Вынес информацию о студенте в отдельную структуру и убрал подключение неимпользуемых модулей

--- prog/lr9.cpp
+++ prog/lr9.cpp
@@ -1,39 +1,43 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <string>
-#include <utility>
 
 using namespace std;
 
-typedef struct StudentList
+typedef struct
 {
     string surname;
-    int *grades{};
-    StudentList *next{};
-} StudentList;
+    int grades[4];
+} Student;
 
-void addStudent(StudentList **head, string surname, int *grade);
-void sortBySurnames(StudentList **studentsArr, int size);
+typedef struct OneWayList
+{
+    Student data;
+    OneWayList *next{};
+} OneWayList;
+
+void addStudent(OneWayList **head, Student student);
+void sortBySurnames(OneWayList **studentsArr, int size);
 
 int main()
 {
     srand(time(nullptr));
     // Задание 1
-    StudentList *students = nullptr;
+    OneWayList *students = nullptr;
     string surnames[] = {"Сперанский", "Калашникова", "Фридрих", "Гуляев", "Брунилин", "Шильников", "Щукин", "Аникеев", "Цибулевич", "Халиков"};
     //  Генерируем список студентов
-    for (const auto& surnameBuff : surnames)
+    for (const auto &surnameBuff : surnames)
     {
-        int *gradeBuff = new int[4];
+        Student dataBuffer;
         for (int j = 0; j < 4; j++)
         {
-            gradeBuff[j] = rand() % 4 + 2;
+            dataBuffer.grades[j] = rand() % 4 + 2;
         }
-        addStudent(&students, surnameBuff, gradeBuff);
+        dataBuffer.surname = surnameBuff;
+        addStudent(&students, dataBuffer);
     }
-    auto **indexArr = new StudentList *[10];
-    StudentList *currentStudent = students;
+    auto **indexArr = new OneWayList *[10];
+    OneWayList *currentStudent = students;
     for (int i = 0; i < 10; i++)
     {
         indexArr[i] = currentStudent;
@@ -42,33 +46,36 @@ int main()
     sortBySurnames(indexArr, 10);
     for (int i = 0; i < 10; i++)
     {
-        cout << (*indexArr[i]).surname << "   ";
+        cout << (*indexArr[i]).data.surname << "   ";
         for (int j = 0; j < 4; j++)
-            cout << (*indexArr[i]).grades[j] << ' ';
+            cout << (*indexArr[i]).data.grades[j] << ' ';
         cout << endl;
     }
     free(indexArr);
     return 0;
 }
 
-void addStudent(StudentList **head, string surname, int *grades)
+void addStudent(OneWayList **head, Student student)
 {
-    auto *temp = new StudentList;
+    auto *temp = new OneWayList;
     temp->next = *head;
-    temp->grades = grades;
-    temp->surname = std::move(surname);
+    for (int i = 0; i < 4; i++)
+    {
+        temp->data.grades[i] = student.grades[i];
+    }
+    temp->data.surname = std::move(student.surname);
     *head = temp;
 }
 
-void sortBySurnames(StudentList **studentsArr, int size)
+void sortBySurnames(OneWayList **studentsArr, int size)
 {
-    StudentList *temp;
+    OneWayList *temp;
     for (int i = 0; i < size - 1; i++)
     {
         int k = i;
         for (int j = i + 1; j < size; j++)
         {
-            if ((studentsArr[j])->surname < (studentsArr[k])->surname)
+            if ((studentsArr[j])->data.surname < (studentsArr[k])->data.surname)
             {
                 k = j;
             }
