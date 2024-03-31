f74f6c9766c67b23c5c6588c79d541256f0804b3
SperanskyArtyom <awesomepinkieguy@gmail.com>
2024-03-31 07:23:52 +0700

    Поменял вывод

--- saod/lr1.8/main.cpp
+++ saod/lr1.8/main.cpp
@@ -16,6 +16,9 @@ void printPhoneDict (const phoneEntry dict[], size_t size);
 void printPhoneDict(const phoneEntry dict[], size_t size, const int indexArr[]);
 void printPhoneDict(const phoneEntry dict[], const vector<int>& indexArr);
 void SortTelephoneDictionary(const phoneEntry dict[], size_t size, int *indexArr, bool (*compare)(const phoneEntry &, const phoneEntry &));
+void FillIndex(int* IndexArr, int size);
+void printMass(int* Arr, int size);
+
 
 bool lessName(const phoneEntry& a, const phoneEntry& b);
 bool lessStreet(const phoneEntry& a, const phoneEntry& b);
@@ -32,18 +35,28 @@ int main() {
             {"Banina Natalia Valerievna","+7-888-666-55-22", "Kalininskiy", "Kropotkina, 138"},
             {"Speransky Artem Alexandrovich",     "+7-777-666-55-33", "Kalininskiy", "Kropotkina, 120"}
     };
-    int sortedIndexesName[N], sortedIndexesStreet[N];
-    SortTelephoneDictionary(dict, N, sortedIndexesName, lessName);
-    SortTelephoneDictionary(dict, N, sortedIndexesStreet, lessStreet);
+    int sortedIndexesName[N], sortedIndexesStreet[N], IndexArr[N];
+    FillIndex(sortedIndexesName, N);
+    FillIndex(sortedIndexesStreet, N);
+    FillIndex(IndexArr, N);
 
     cout << "Неотсортированный справочник:\n";
     printPhoneDict(dict, N);
 
+    SortTelephoneDictionary(dict, N, sortedIndexesName, lessName);
     cout << "\nОтсортированный по ФИО справочник:\n";
     printPhoneDict(dict, N, sortedIndexesName);
 
+    SortTelephoneDictionary(dict, N, sortedIndexesStreet, lessStreet);
     cout << "\nОтсортированный по адресу справочник:\n";
     printPhoneDict(dict, N, sortedIndexesStreet);
+
+    cout << "\nИндексный массив до сортировки: ";
+    printMass(IndexArr, N);
+    cout << "Индексный массив после сортировки по имени: ";
+    printMass(sortedIndexesName, N);
+    cout << "Индексный массив после сортировки по адресу: ";
+    printMass(sortedIndexesStreet, N);
 
     string field, key;
     cout << "\nПо какому полю произвести поиск: ";
@@ -77,8 +90,6 @@ int main() {
 
 void SortTelephoneDictionary(const phoneEntry dict[], size_t size, int *indexArr, bool (*compare)(const phoneEntry &, const phoneEntry &))
 {
-    for (int i = 0; i < size; i++)
-        indexArr[i] = i;
     for (int i = 1; i < size; i++)
     {
         auto temp = indexArr[i];
@@ -147,4 +158,17 @@ vector<int> BSearchAllName (const phoneEntry dict[], const int indexArr[], size_
     }
 
     return indexes;
+}
+
+void FillIndex(int* IndexArr, int size)
+{
+    for (int i = 0; i < size; i++)
+        IndexArr[i] = i;
+}
+
+void printMass(int* Arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << Arr[i] << ' ';
+    cout << endl;
 }
